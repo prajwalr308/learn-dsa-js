beb3d08fca23fb13edd405f777ab4709ed9238f5
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2025-10-22 21:21:50 +0000

    two sum sorted

---
+++ patterns/two-pointer/two-sum-2/twoSum2.js
@@ -0,0 +1,14 @@
+export function twoSum2(numbers, target) {
+  let start = 0;
+  let end = numbers.length - 1;
+  while (start < end) {
+    if (numbers[start] + numbers[end] === target) {
+      return [start + 1, end + 1];
+    }
+    if (numbers[start] + numbers[end] < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+}
